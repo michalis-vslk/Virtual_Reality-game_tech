905b474aeb5ccdc13600121db0170d99136d9c25
PatroklosGeorgiadis <61284594+PatroklosGeorgiadis@users.noreply.github.com>
2023-02-14 11:34:29 +0200

    Improving text quality and title trigger

--- Virtual_Reality/Assets/TriggersForMovement.cs
+++ Virtual_Reality/Assets/TriggersForMovement.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class TriggersForMovement : MonoBehaviour
 {
     [SerializeField] UnityEvent onPlayerTrigger;
+    public GameObject bg;
+    public Image imgbg;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bg = GameObject.Find("/Canvas/TextBG");
+        imgbg = bg.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -22,9 +26,18 @@ public class TriggersForMovement : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            UIAreaTitle.Instance.ShowMessage("Village");
+            UIAreaTitle.Instance.ShowMessage("The Village");
+            imgbg.color = new Color32(10, 10, 10, 240);
 
+            StartCoroutine(passiveMe(5));
             onPlayerTrigger.Invoke();
         }
+    }
+ 
+    IEnumerator passiveMe(int secs)
+    {
+        yield return new WaitForSeconds(secs);
+        UIAreaTitle.Instance.ShowMessage("");
+        imgbg.color = new Color32(10, 10, 10, 0);
     }
 }
